4306654e10632c3aa0c079b8aa95d145217aac51
abdullateef suleiman <sabdullateef87@gmail.com>
2023-02-24 13:28:50 +0100

    j

---
+++ src/pages/landing_page/footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const social = [
+    {
+        name: "Facebook",
+        icon: ""
+    },
+    {
+        name: "Instagram",
+        icon: ""
+    },
+    {
+        name: "Twitter",
+        icon: ""
+    },
+]
